1cff8c3073a33e2d1e1c670d2019f3467e24097f
LuciaSolesio <127357551+LuciaSolesio@users.noreply.github.com>
2023-05-11 19:50:23 -0300

    Add files via upload

--- divisores.ts
+++ divisores.ts
@@ -1,35 +1,27 @@
-/* IMPLEMENTE UN METODO LLAMADO cantidadDeDivisores QUE RECIBA UN NUMERO ENTERO Y DEVUELVA LA CANTIDAD DE DIVISORES.
-POR EJ, PARA EL NUMERO 16
+/* Implemente un metodo llamado cantidadDeDivisores que reciba un numero entero y devuelva la cantidad de divisores. 
+Por ejemplo para el numero 16  sus divisores son 1, 2, 4, 8, 16, por lo cual la respuesta deberia ser 5.
+Re-utilice el metodo esMultiplo implementado para el ejercicio anterior.
 */
 
 import * as rls from "readline-sync";
 
 let dividendo : number = rls.questionInt("Ingrese un dividendo: ");
 let divisor : number = 0;
-let cantMulti : number = 0;
+let cantDiv : number = 0;
 
-
-for (divisor = 1; divisor <= dividendo; divisor ++) {
-    if (esMultiplo(dividendo,divisor)) {
-        cantMulti++;
-        console.log(divisor)
-    }
-}
-
-function esDivisor(dividendo:number, divisor:number): boolean {
-    
-    if (dividendo % divisor == 0){
+function esMultiplo(divisor: number, dividendo: number): boolean {
+    if (divisor % dividendo == 0) {
         return true;
     } else {
         return false;
     }
 }
 
-function esMultiplo(num1: number, num2: number): boolean {
-    if (num1 % num2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
+function cantidadDeDivisores (divisor: number, dividendo:number): number {
+    for (divisor = 1; divisor <= dividendo; divisor ++) {
+    if (esMultiplo(dividendo,divisor) == true) {
+        cantDiv++;
+    } 
+} return cantDiv;
 }
- 
+console.log(`La cantidad de divisores de ${dividendo} es: `, cantidadDeDivisores(divisor, dividendo));
